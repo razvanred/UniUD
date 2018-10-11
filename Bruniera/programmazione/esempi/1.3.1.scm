7ce528b7166a0af20417b4cac114d13db7a6b3b5
KayJay7 <alvisebruniera@gmail.com>
2018-10-11 17:16:56 +0200

    programmazione fine 11/10/18

--- Bruniera/programmazione/esempi/1.3.1.scm
+++ Bruniera/programmazione/esempi/1.3.1.scm
@@ -1,5 +1,8 @@
-(define sup-tot-cil
-  (lambda (rb h)
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1.3.1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define sup-tot-cil               ; val: misura
+  (lambda (rb h)                  ; rb, h: misure
     (* 2 (* pi (* rb (+ h rb))))
     )
   )
--- Bruniera/programmazione/esempi/1.3.2.scm
+++ Bruniera/programmazione/esempi/1.3.2.scm
@@ -1,8 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1.3.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define sm-plurale              ; val: nme plurale
-  (lambda (p)                   ; p: nome da pluralizzare
+(define sm-plurale              ; val: stringa nome plurale
+  (lambda (p)                   ; p: stringa nome da pluralizzare
     (string-append
      (substring
       p
@@ -12,3 +12,11 @@
      "i")                       ; desinenza plurale
     )
   )
+
+(define last-char-comparator(word, char)
+  (char=?
+   (string-ref
+    word
+    (- (string-length word) 1)
+    )
+   #\a))
