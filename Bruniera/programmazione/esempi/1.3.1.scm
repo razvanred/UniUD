01229f5ff09497df10536925f61daaf28769ba65
KayJay7 <alvisebruniera@gmail.com>
2018-10-11 16:17:56 +0200

    programmazione pausa 11/10/18

--- Bruniera/programmazione/esempi/1.3.1.scm
+++ Bruniera/programmazione/esempi/1.3.1.scm
@@ -1,4 +1,3 @@
-#lang racket
 (define sup-tot-cil
   (lambda (rb h)
     (* 2 (* pi (* rb (+ h rb))))
--- Bruniera/programmazione/esempi/1.3.2.scm
+++ Bruniera/programmazione/esempi/1.3.2.scm
@@ -1,2 +1,14 @@
-#lang racket
-(string-append (substring p 0 (length p)) "i")
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1.3.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define sm-plurale              ; val: nme plurale
+  (lambda (p)                   ; p: nome da pluralizzare
+    (string-append
+     (substring
+      p
+      0                         ; prima lettera
+      (- (string-length p) 1)   ; lunghezza della radice
+      )
+     "i")                       ; desinenza plurale
+    )
+  )
