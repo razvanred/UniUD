4c9e5bf76a3f397e18d395d50dd0f82d018d5c80
KayJay7 <alvisebruniera@gmail.com>
2018-10-16 11:35:24 +0200

    completato esempio di programmazione 1.3.2.scm

--- Bruniera/programmazione/esempi/1.3.2.scm
+++ Bruniera/programmazione/esempi/1.3.2.scm
@@ -1,8 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1.3.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define sm-plurale              ; val: stringa nome plurale
-  (lambda (p)                   ; p: stringa nome da pluralizzare
+#reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname 1.3.2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(define sm-plurale              ; val: stringa plurale
+  (lambda (p)                   ; p: stringa nome maschile da pluralizzare
     (string-append
      (substring
       p
@@ -13,10 +13,37 @@
     )
   )
 
-(define last-char-comparator(word, char)
-  (char=?
-   (string-ref
-    word
-    (- (string-length word) 1)
+(define sf-plurale              ; val: stringa nome plurale
+  (lambda (p)                   ; p: stringa nome femminile da pluralizzare
+    (string-append
+     (substring
+      p
+      0                         ; prima lettera
+      (- (string-length p) 1)   ; lunghezza della radice
+      )
+     "e")                       ; desinenza plurale
     )
-   #\a))
+  )
+
+(define last-char-comparator    ; boolean: l'ultimo carattere della lettera è uguale a quello passato
+  (lambda (word char)           ; word: stringa di qui prendere l'ultimo carattere ; char: carattere da controllare
+    (char=?                     ; sono uguali...
+     (string-ref
+      word
+      (- (string-length word) 1); l'ultimo carattere,
+      )
+     char)                      ; e la lettera?
+    )
+  )
+
+(define s-plurale               ; val: stringa plurale
+  (lambda (nome)                ; nome: stringa da pluralizzare
+    (if (last-char-comparator   ; se la parola finisce con 'a'
+         nome 
+         #\a
+         )
+        (sf-plurale nome)       ; trattalo come femminile
+        (sm-plurale nome)       ; altrimenti come maschile
+        )
+    )
+  )
