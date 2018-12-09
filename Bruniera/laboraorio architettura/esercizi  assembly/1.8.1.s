4fe4feb8fd593c3bfc5cbfdc19f3f9c098fbc796
KayJay7 <alvisebruniera@gmail.com>
2018-12-09 15:24:11 +0100

    aggiunte immagini della serrimana del 20/11/18 completato problema 1.8 di programmazione laboratorio corretto errore negli appunti del 6/12/18 migliorata soluzione del problema 1.8.1 di programmazione laboratorio

--- Bruniera/laboraorio architettura/esercizi  assembly/1.8.1.s
+++ Bruniera/laboraorio architettura/esercizi  assembly/1.8.1.s
@@ -1,24 +1,21 @@
 .data
 n1: .word -18
-n2: .word 20
-n3: .word -3
-n4: .word 12
+n2: .word 6
+n3: .word -5
+n4: .word 35
 .text
-es1:    ldr r8, =n1 @carico l'indirizzo del primo numero
-        ldr r0, [r8] @carico il primo numero
-        ldr r2, [r8, #4]
-        add r0, r0, r2 @sommo primo e secondo
-        ldr r2, [r8, #8]
-        add r0, r0, r2 @sommo il terzo
-        ldr r2, [r8, #12]
-        add r0, r0, r2 @sommo il quarto
+es1:    ldr r4, =n1 @carico l'indirizzo del primo numero
+        ldr r0, [r4, #4] @carico il secondo numero
+        ldr r1, [r4, #8]
+        add r0, r0, r1 @sommo secondo e terzo
+        ldr r1, [r4, #12]
+        add r0, r0, r1 @sommo il qarto
+        ldr r4, [r4] @carico il primo per tenerlo
+        add r0, r0, r4 @sommo il quarto
 es2:    mov r1, r0, lsr #2 @shifto a destra per dividere
-es3:    ldr r3, [r8]
-        add r2, r3, r3, lsl #10 @n1+n1*2^10
-es4:    mov r5, #15 @creo una maschera degli ultimi 4 bit
-        ldr r4, [r8]
-        and r4, r4, r5
-es5:    ldr r5, [r8]
-        mov r5, r5, lsr #31
+es3:    add r2, r4, r4, lsl #10 @n1+n1*2^10
+es4:    mov r3, #15 @creo una maschera degli ultimi 4 bit
+        and r3, r3, r4
+es5:    mov r4, r4, lsr #31
 end:    swi 0x11
 .end
--- Bruniera/laboratorio programmazione/esercizi/1.8.rkt
+++ Bruniera/laboratorio programmazione/esercizi/1.8.rkt
@@ -12,8 +12,8 @@
         (list (list s a) (list d b) (list t c))
         (let ((nn (- n 1)))
           (if (< k (expt 2 nn))
-              (hanoi-disks-rec nn k s d t (+ a 1) b c)
-              (hanoi-disks-rec nn k t d s c (+ b 1) a))))
+              (hanoi-disks-rec nn k s t d (+ a 1) c b)
+              (hanoi-disks-rec nn (- k (expt 2 nn)) t d s c (+ b 1) a))))
     ))
 
 (hanoi-disks 3 0)
@@ -27,32 +27,27 @@
 (hanoi-disks 5 13)
 (hanoi-disks 15 19705)
 (hanoi-disks 15 32767)
-
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (define hanoi-picture
   (lambda (n k)
-    (hanoi-disks-rec n k 1 2 3 0 0 0)
+    (hanoi-picture-rec n k 1 2 3 0 0 0 (towers-background n))
     ))
 
 (define hanoi-picture-rec
-  (lambda (n k s d t a b c)
+  (lambda (n k s d t a b c i)
     (if (= n 0)
-        (list (list s (+ n a)) (list d b) (list t c))
-        (let ((nn (- n 1)))
+        i
+        (let ((nn (- n 1))
+              (all (+ a (+ b (+ c n)))))
           (if (< k (expt 2 nn))
-              (hanoi-picture-rec nn k s t d (+ a 1) c b)
-              (hanoi-picture-rec nn k t d s c (+ b 1) a))))
+              (hanoi-picture-rec nn k s t d (+ a 1) c b
+                                 (above (disk-image n all s a) i))
+              (hanoi-picture-rec nn (- k (expt 2 nn)) t d s c (+ b 1) a
+                                 (above (disk-image n all d b) i)))))
     ))
 
-(hanoi-picture 3 0)
-(hanoi-picture 3 1)
-(hanoi-picture 3 2)
-(hanoi-picture 3 3)
-(hanoi-picture 3 4)
-(hanoi-picture 3 5) 
-(hanoi-picture 3 6)
-(hanoi-picture 3 7)
+(hanoi-picture 5 0)
 (hanoi-picture 5 13)
+(hanoi-picture 5 22)
+(hanoi-picture 5 31)
 (hanoi-picture 15 19705)
-(hanoi-picture 15 32767)
