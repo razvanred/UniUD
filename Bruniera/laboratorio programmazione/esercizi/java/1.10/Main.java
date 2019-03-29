8e9761a45e0343e95f8c587fec28f56300a94c1a
KayJay7 <alvisebruniera@gmail.com>
2019-03-29 15:45:05 +0100

    programmazione laboratorio 29/03/19

---
+++ Bruniera/laboratorio programmazione/esercizi/java/1.10/Main.java
@@ -0,0 +1,38 @@
+public class Main{
+  public static void main(String[] argc){
+   System.out.println(btrSucc("+.-+-.")); 
+   System.out.println(btrSucc("+.-+--"));
+   System.out.println(btrSucc("+.-.++"));
+   System.out.println(onesComplement2("101000110110101"));
+   System.out.println(onesComplement2("001110110001011"));
+   System.out.println(onesComplement("101000110110101"));
+   System.out.println(onesComplement("001110110001011"));
+  }
+  
+  public static String btrSucc(String btr){
+    if(btr.charAt(btr.length()-1)=='-'){
+      return btr.substring(0,btr.length()-1)+".";
+    } else if(btr.charAt(btr.length()-1)=='.'){
+      return btr.substring(0,btr.length()-1)+"+";
+    }
+    return btrSucc(btr.substring(0,btr.length()-1))+"-";
+  }
+  
+  public static String onesComplement(String bin){
+    String a="";
+    for(int i=0;i<bin.length();i++){
+      if(bin.charAt(i)=='0'){
+        a+="1";
+      } else {
+        a+="0";
+      }
+    }
+    return a;
+  }
+  
+   public static String onesComplement2(String bin){
+    return bin.replace('0','.')
+      .replace('1','0')
+      .replace('.','1');
+  }
+}
