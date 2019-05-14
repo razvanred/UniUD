88d2b079f5f7f049cf51f9637040e3e70b45157f
KayJay7 <alvisebruniera@gmail.com>
2019-05-14 17:17:14 +0200

    programmazione 14/5/19

--- Bruniera/programmazione/esempi/java/1.15/Huffman.java
+++ Bruniera/programmazione/esempi/java/1.15/Huffman.java
@@ -39,11 +39,30 @@ public class Huffman{
     }
     
     while(q.size()>1){
-      HNode r=q.poll();
       HNode l=q.poll();
+      HNode r=q.poll();
       q.add(new HNode(l,r));
     }
     return q.poll();
   }
   
+  public static void comprimi(String doc, String com){
+    int[] tab=tabOccorrenze(doc);
+    HNode t=hAlbero(tab);
+    int count=t.weight();
+    String[] codes=t.codesTab();
+    InputTextFile itf=new InputTextFile(doc);
+    OutputTextFile otf=new OutputTextFile(com);
+    
+    otf.writeTextLine(""+t.weight());
+    otf.writeTextLine(t.codAlbero());
+    
+    for(int i=0;i<count;i++){
+      char c=itf.readChar();
+      
+      otf.writeCode(codes[c]);
+    }
+    itf.close();
+    otf.close();
+  }
 }
