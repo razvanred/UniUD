e344b092413a089a3b45b769877397d78e905f0e
razvan <razvanred99@gmail.com>
2019-06-12 00:20:59 +0200

    Finito il 10^ problema di programmazione laboratorio

---
+++ Rosu/programmazione/laboratorio/java/problemi/problema10/src/ro/razvan/uniud/stringSList/Principale.kt
@@ -0,0 +1,35 @@
+package ro.razvan.uniud.stringSList
+
+fun main() {
+    println("testing empty constructor and length: ${StringSList().length == 0}")
+    println(
+        "testing primary constructor and cons: ${StringSList(
+            "sono",
+            StringSList("razvan")
+        ) == StringSList("razvan").cons("sono")}"
+    )
+    println("testing toString with empty list: ${StringSList.NULL_STRING_S_LIST.toString() == "()"}")
+    println("testing toString with list: ${StringSList("razvan").toString() == "(razvan)"}")
+    println("testing length: ${StringSList("razvan").cons("rosu").length == 2}")
+    println("testing car: ${StringSList("madalina", StringSList("razvan")).car == "madalina"}")
+    println(
+        "testing cdr: ${StringSList(
+            "stefan",
+            StringSList("rosu").cons("alexandru")
+        ).cdr == StringSList("rosu").cons("alexandru")}"
+    )
+    println("testing listRef: ${StringSList("world").cons("hello").listRef(1) == "world"}")
+
+    val listA = StringSList("razvan", StringSList("rosu"))
+    val listB = StringSList("emma", StringSList("rosu", StringSList("violetta")))
+
+    println("testing append: ${listA.append(listB).toString() == "(razvan, rosu, emma, rosu, violetta)"}")
+    println("testing reverse: ${listB.reverse().toString() == "(violetta, rosu, emma)"}")
+
+    println(
+        "testing btrNotation: ${BtrNotation.btrSucc(
+            "-",
+            5
+        ) == StringSList("+.").cons("+-").cons("+").cons(".").cons("-")}"
+    )
+}
