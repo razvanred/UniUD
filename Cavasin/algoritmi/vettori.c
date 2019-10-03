29e7071beb66d0a2e59c6b9fc8ef3901cc013e72
Nemo-LUC <artemiscode@hotmail.it>
2019-10-03 11:40:10 +0200

    set up vs code workspace to support msvc

--- Cavasin/algoritmi/vettori.c
+++ Cavasin/algoritmi/vettori.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void printArray(int v[], int size) {
   printf("{");
-  for(int i= 0; i < size; i++) {
+  for (int i = 0; i < size; i++) {
     printf(" %d", v[i]);
   }
   printf(" }\n");
 }
 
 void insertionSort(int v[], int size) {
-  printArray(v, size);
-  for(int t, i, j= 1; j < size; j++) {
-    t= v[j];
-    for(i= j - 1; i >= 0 && v[i] > t; i--) {
-      v[i + 1]= v[i];
+  for (int t, i, j = 1; j < size; j++) {
+    t = v[j];
+    for (i = j - 1; i >= 0 && v[i] > t; i--) {
+      v[i + 1] = v[i];
     }
-    v[i + 1]= t;
-    printArray(v, size);
+    v[i + 1] = t;
   }
 }
 
+void mergeSort() {}
+
 int main() {
-  int v[6]= {5, 3, 2, 4, 8, 1};
+  int v[6], t[6] = {5, 3, 2, 4, 8, 1};
 
   insertionSort(v, 6);
+  printArray(v, 6);
 
   return 0;
 }
