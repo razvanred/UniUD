cec8df20932f5e9f1b717045b7cb0a729c896a06
Firamon <calabrigo.massimo@gmail.com>
2019-10-22 11:02:19 +0200

    Added the first OOP exercise

---
+++ Calabrigo/OOP/EserciziOOP/EsercizioSIM/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioSIM {
+    class Program {
+        static void Main(string[] args) {
+            SIMHandler sH = new SIMHandler();
+            String OPERATOR = "operator1";
+            SIM s1 = sH.createSIM("3276319633", 10000, null,
+                new date(10,10,2019), "tizio",
+                promotions.NOPROMOTION, OPERATOR);
+            SIM s2 = sH.createSIM("380438648", 10000, null,
+                new date(8, 3, 2017), "caio",
+                promotions.CHIAMAERICHIAMA, OPERATOR);
+            SIM s3 = sH.createSIM("0438738374", 10000, null,
+                new date(6, 4, 2018), "semprogno",
+                promotions.MINUTIILLIMITATI, "operator2");
+            sH.phone(s1, "380438648");
+            sH.phone(s1, "0438738374");
+            sH.phone(s2, "380438648");
+            sH.phone(s3, "380438648");
+            sH.phone(s3, "3276319633");
+            sH.phone(s2, "3276319633");
+            sH.registerInFile(s1);
+            sH.registerInFile(s2);
+            sH.registerInFile(s3);
+        }
+    }
+}
---
+++ Calabrigo/OOP/EserciziOOP/EsercizioSIM/SIM.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+/*
+ • il numero di telefono
+• il codice PUK
+• il credito disponibile in euro
+• la lista delle telefonate effettuate
+• la data dell’ultima ricarica
+• il proprietario
+• eventuali promozioni attive
+-promozioni:
+    - MINUTI ILLIMITATI, con costo di attivazione di 10 euro
+- CHIAMA E RICHIAMA, con costo di attivazione di 15 euro
+- DATI UNLIMITED, senza costi di attivazione
+*/
+namespace EsercizioSIM {
+
+    struct date {
+        int day;
+        int month;
+        int year;
+
+        public date(int day, int month, int year) {
+            this.day = day;
+            this.month = month;
+            this.year = year;
+        }
+
+        public bool hasOneYearPassed(date past, date present) {
+            int daysPassed = 0;
+            int monthsPassed = ((present.year - past.year) * 12) + (present.month - past.month);
+            daysPassed = (present.day - past.day) + (monthsPassed * 30);
+            return daysPassed >= 365 ? true : false;
+        }
+
+        public String getDateEuropeanFormat() {
+            return "" + day + "/" + month + "/" + year;
+        }
+    }
+
+    struct call {
+        int minutes;
+        String telephoneNum;
+
+        public call(int minutes, String telephoneNum) {
+            this.minutes = minutes;
+            this.telephoneNum = telephoneNum;
+        }
+
+        public int getMinutes() {
+            return this.minutes;
+        }
+
+        public String getTelNumber() {
+            return this.telephoneNum;
+        }
+    }
+
+    enum promotions {
+        NOPROMOTION = 0,
+        MINUTIILLIMITATI = 1,
+        CHIAMAERICHIAMA = 2,
+        DATIUNLIMITED = 3
+    }
+
+    class SIM {
+        private String lastOperator;
+        private String telNum;
+        private double avaibleCredit;
+        private List<call> calls;
+        private date date;
+        private String ownerNickname;
+        private promotions promotion;
+
+        //SIM attivata con promozione
+        public SIM(String telNum, double avaibleCredit
+            , date date, String ownerNickname, promotions promotion) {
+            this.telNum = telNum;
+            this.avaibleCredit = avaibleCredit;
+            this.calls = new List<call>();
+            this.date = date;
+            this.ownerNickname = ownerNickname;
+            this.promotion = promotion;
+            this.lastOperator = "operator1";
+        }
+
+        //SIM attivata con portabilità
+        public SIM(String telNum, double avaibleCredit, List<call> calls
+            , date date, String ownerNickname, promotions promotion, String lastOperator) {
+            this.telNum = telNum;
+            this.avaibleCredit = avaibleCredit;
+            this.calls = new List<call>();
+            this.date = date;
+            this.ownerNickname = ownerNickname;
+            this.promotion = promotion;
+            this.lastOperator = lastOperator;
+        }
+
+        public int phone(String telephoneNum) {
+            Random r = new Random();
+            int callMin = r.Next(1, 100);
+            //call only if you have enought credit
+            if ((avaibleCredit - (callMin * 2)) > 0) {
+                call c = new call(callMin, telephoneNum);
+                calls.Add(c);
+                if ((promotion == promotions.NOPROMOTION) || (promotion == promotions.DATIUNLIMITED))
+                    this.avaibleCredit -= (callMin * 2);
+                return callMin;
+            }
+            return 0;
+        }
+
+        public void minutiIlliminati() {
+            this.promotion = promotions.MINUTIILLIMITATI;
+        }
+
+        public void chiamaERichiama() {
+            this.promotion = promotions.CHIAMAERICHIAMA;
+        }
+
+        public void datiUnlimited() {
+            this.promotion = promotions.DATIUNLIMITED;
+        }
+
+        public List<call> getCalls() {
+            return this.calls;
+        }
+
+        public void setPromotion(promotions promotion) {
+            this.promotion = promotion;
+        }
+
+        public String getOperator() {
+            return this.lastOperator;
+        }
+
+        public date getDate() {
+            return this.date;
+        }
+
+        public String getTelNumber() {
+            return this.telNum;
+        }
+
+        public String getOwnerNickname() {
+            return this.ownerNickname;
+        }
+
+        public double getAvaibleCredit() {
+            return this.avaibleCredit;
+        }
+    }
+}
