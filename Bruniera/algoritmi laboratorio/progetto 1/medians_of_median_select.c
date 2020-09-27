69e27d39388774e2a9b9648409754276a6188b25
KayJay7 <alvisebruniera@gmail.com>
2020-09-27 11:48:21 +0200

    corretto medians of medians

--- Bruniera/algoritmi laboratorio/progetto 1/medians_of_median_select.c
+++ Bruniera/algoritmi laboratorio/progetto 1/medians_of_median_select.c
@@ -14,13 +14,14 @@ int main(int argc, char** argv) {
     unsigned int size; 
     char str[10000];
     unsigned int k;
-    
+    //leggo l'array, la posizione, ed eseguo la ricerca
 	readline(str, 10000);
     parseArray(&v, &size, str);
     scanf("%d", &k);
     printf("%d\n", median_of_medians_select(v, 0, size - 1, k - 1));
 }
 
+//selezione inefficiente basata su selection_sort
 int cubic_selection(int array[], int lower, int upper, int index) {
 	int selected;// = lower;
 	
@@ -38,24 +39,35 @@ int cubic_selection(int array[], int lower, int upper, int index) {
 }
 
 int median_of_medians(int array[], int lower, int upper) {
+	//termina se l'array è singoletto
     if(lower == upper) {
         return array[upper];
     }
     int times = (upper - lower) / 5;
     int last = upper;
     
+    //trovo il mediano dell'ultima partizione e lo metto in ultima posizione (perché potrebbe avere meno di 5 elementi)
     cubic_selection(array, lower + (5 * times), upper,  upper - (((upper - lower) % 5) / 2));
     swap(array,upper - (((upper - lower) % 5) / 2), last);
     last--;
+    
+    //trovo il mediano delle restanti partizioni (in ordine decrescente)
     for(int i = times - 1; i >= 0; i--) {
         cubic_selection(array, lower + (5 * i), lower + (5 * i) + 4, lower + (5 * i) + 2);
+        //sposto i mediani alla fine dell'array
         swap(array, lower + (5 * i) + 2, last);
         last--;
     }
     
-    return median_of_medians(array, upper - times, upper);
+    //trovo il mediano tra i mediani (accumulati in fondo all'array)
+    median_of_medians_select(array, upper - times, upper, upper - (times / 2));
+    //porto il mediano in ultima posizione
+	swap(array, upper - (times / 2), upper);
+	//restituisco il mediano
+	return array[upper];
 }
 
+//leggo la riga come stringa
 void readline(char s[], unsigned int size) {
   unsigned int i= 0;
   char c;
@@ -78,6 +90,7 @@ void readline(char s[], unsigned int size) {
   }
 }
 
+//produco un vettore dalla stringa
 void parseArray(int **v, unsigned int *size, char s[]) {
   *size= 0;
   for(unsigned int i= 0; s[i] != 0; i++) {
@@ -93,16 +106,19 @@ void parseArray(int **v, unsigned int *size, char s[]) {
   }
 }
 
-int partition(int array[], int lower, int upper) {
-	median_of_medians(array, lower, upper);
+int partition(int array[], int lower, int upper) {	
+	//swap(array, k, upper); //swap non necessario, scelgo sempre l'ultimo come pivot
 	int break_even = lower - 1;
+	
 	for(int i = lower; i <= upper; i++){
+		//eseguo lo scambio se l'elemento è più grande dell'ultimo
 		if(array[i] <= array[upper]){
 			break_even++;
 			swap(array, break_even, i);
 		}
 	}	
 	
+	//restituisco il pivot
 	return break_even;
 }
 
@@ -113,17 +129,25 @@ void swap(int array[], int first, int second) {
 }
 
 int median_of_medians_select(int array[], int lower, int upper, int k) {
+	//termina se l'array è vuoto
 	if(lower > k || upper < k) {
 		return -1;
 	}
+	median_of_medians(array, lower, upper);
+	//medians_of_medians porta il mediano in ultima posizione, pronto per il partizionamento
+	//partiziono l'array con il mediano dei mediani come pivot
 	int break_even = partition(array, lower, upper);
 	
 	if(k == break_even) {
+		//se il pivot è la posizione che cerco restituisco l'elemento
 		return array[k];
 	} else if(k > break_even) {
+		//altrimenti ricorro sulla partizione che contiene la posizione
+		//la partizione avrà dimensione al più n*7/10 grazie alla scelta del pivot
 		return median_of_medians_select(array, break_even + 1, upper, k);
 	} else {
 		return median_of_medians_select(array, lower, break_even - 1, k);
 	}
+	//n.b.: al termine l'elemento selezionato si trova nella posizione k
 }
 
