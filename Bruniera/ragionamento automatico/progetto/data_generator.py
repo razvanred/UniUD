3ea985b3f82f6ff4d8016d9c318051dba421564e
KayJay7 <31775749+KayJay7@users.noreply.github.com>
2023-03-21 17:28:13 +0100

    generati benchmark per il progetto di AR

--- Bruniera/ragionamento automatico/progetto/data_generator.py
+++ Bruniera/ragionamento automatico/progetto/data_generator.py
@@ -1,6 +1,7 @@
 #!/bin/env python3
 
 import random
+import sys
 
 def random_graph(size):
     zinc="["
@@ -8,7 +9,7 @@ def random_graph(size):
     for i in range(0,size):
         zinc += "|"
         for j in range(0,size):
-            length=random.randint(-22,20)
+            length=random.randint(-25,20)
             if length >= 0 and i != j:
                 zinc += f"{length},"
                 asp += f"strada({i},{j},{length}).\n"
@@ -18,6 +19,26 @@ def random_graph(size):
     zinc += "|]\n"
     return (zinc,asp)
 
-graph = random_graph(13)
-print(graph[0])
-print(graph[1])
+def generate(size, name):
+    graph = random_graph(size)
+    f = open(f"benchmark/minizinc/{name}.dzn", "w+")
+    f.write(f"start = 1;\nlimit = 6;\nn = {size};\nm={graph[0]};")
+    f.close()
+    f = open(f"benchmark/asp/{name}.lp", "w+")
+    f.write(f"start(0).\nlimit(6).\nn({size}).\n{graph[1]}")
+    f.close()
+
+def main():
+    generate(6, "easy1")
+    generate(10, "easy2")
+    generate(12, "easy3")
+
+    for i in range(1,31):
+        generate(14, f"mid{i}")
+
+    generate(30, "hard1")
+    generate(50, "hard2")
+    generate(100, "hard3")
+
+if __name__ == "__main__":
+    main()
