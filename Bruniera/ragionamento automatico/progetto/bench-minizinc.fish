257690a0ebadac302aac4d4d6d02dc4c6dca2361
KayJay7 <31775749+KayJay7@users.noreply.github.com>
2023-03-22 08:34:38 +0100

    primi benchmark progetto AR

---
+++ Bruniera/ragionamento automatico/progetto/bench-minizinc.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/fish
+function minizinc-run -a file
+    echo $file
+    set output (minizinc --solver org.gecode.gecode minizinc/progetto.mzn $file --output-time --time-limit 300000 2>/dev/null | string split0)
+    set temp (echo -e "$output" | tail --lines 6 | head --lines 2 | string split0)
+    echo -e "$file:\n $temp" >>results/times-minizinc.txt
+    echo -e "$file:\n $output" >>results/solutions-minizinc.txt
+end
+
+
+rm results/*minizinc*
+
+set files (ls benchmark/minizinc/easy*.dzn -v)
+for file in $files
+    minizinc-run $file
+end
+set files (ls benchmark/minizinc/mid*.dzn -v)
+for file in $files
+    minizinc-run $file
+end
+set files (ls benchmark/minizinc/hard*.dzn -v)
+for file in $files
+    minizinc-run $file
+end
