f349d67a62493482826496dca3f8218430344418
KayJay7 <31775749+KayJay7@users.noreply.github.com>
2023-03-22 11:26:24 +0100

    corretta la la formattazione dei benchmark AR

--- Bruniera/ragionamento automatico/progetto/bench-minizinc.fish
+++ Bruniera/ragionamento automatico/progetto/bench-minizinc.fish
@@ -2,9 +2,9 @@
 function minizinc-run -a file
     echo $file
     set output (minizinc --solver org.gecode.gecode minizinc/progetto.mzn $file --output-time --time-limit 300000 2>/dev/null | string split0)
-    set temp (echo -e "$output" | tail --lines 6 | head --lines 2 | string split0)
-    echo -e "$file:\n $temp" >>results/times-minizinc.txt
-    echo -e "$file:\n $output" >>results/solutions-minizinc.txt
+    set temp (echo $output | grep -m2 -e 'elapsed' -e 'total' -e 'UNSAT' | string split0)
+    echo -e "$file:\n$temp" >>results/times-minizinc.txt
+    echo -e "$file:\n$output" >>results/solutions-minizinc.txt
 end
 
 
