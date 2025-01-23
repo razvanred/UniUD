736bb38dd130982b3412012f707299849e5f8e3b
Guio25 <mattiaguio@gmail.com>
2025-01-23 19:20:59 +0100

    Fixed TACgenerator.hs

--- TACEmitter/TACgenerator.hs
+++ TACEmitter/TACgenerator.hs
@@ -11,24 +11,25 @@ out instr = do
     (k, revcode) <- get
     put (k, instr : revcode)
 
-newtemp :: MyMon (Type -> Addr)
+newtemp :: MyMon (Type -> XAddr)
 newtemp = do
     (k, revcode) <- get
     put (k + 1, revcode)
     return $ int2AddrTempName k
 
-int2AddrTempName :: (Type -> Addr)
-int2AddrTempName k = (\t -> Temporary k t)
+int2AddrTempName :: Int -> (Type -> XAddr)
+int2AddrTempName k = Addr . Temporary k 
 
 genCode :: MyMon a -> [ TAC ]
 genCode gen = reverse $ snd $ execState gen (0 ,[])
 
-tacExpr :: Expr ASTData -> MyMon Addr
+tacExpr :: Expr ASTData -> MyMon XAddr
 tacExpr x = case x of
     UnaryOp pos uop expr (TypeChecker t lr instr) -> do
         f <- newtemp
-        let temp = f(t)
-        addr <- tacExpr(expr)
+        let temp = f t
+        addr <- tacExpr expr
         out $ TacUnary temp uop t addr
         return temp
+
 
